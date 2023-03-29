7514c05c74afe27fee1e44ca67323579f369f9c5
Prisca C <prisca.castanier@gmail.com>
2023-03-29 16:33:18 +0200

    feat(string): create to snake case method

---
+++ app/Helpers/StringHandler.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Helpers;
+
+class StringHandler
+{
+    public static function toSnakeCase(string $string): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $string));
+    }
+}
