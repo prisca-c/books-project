224dcec0bdda56d21cfc966e2d42cffb34c3ee80
Prisca C <prisca.castanier@gmail.com>
2023-03-31 11:33:25 +0200

    feat(string): add method to handle pluralize of a string

--- Helpers/StringHandler.php
+++ Helpers/StringHandler.php
@@ -8,4 +8,15 @@ class StringHandler
     {
         return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $string));
     }
+
+    public static function pluralize(string $string): string
+    {
+        if (str_ends_with($string, 'y')) {
+            return substr($string, 0, -1) . 'ies';
+        } elseif (str_ends_with($string, 's')) {
+            return $string;
+        } else {
+            return $string . 's';
+        }
+    }
 }
