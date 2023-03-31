b2adc22045a644f59a1f471af2f03f85f055bace
Prisca C <prisca.castanier@gmail.com>
2023-03-31 09:30:54 +0200

    refactor(routes): move all routes to new Routes.php folder

---
+++ Router/Routes.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Router;
+
+use Exception;
+
+class Routes
+{
+    public static function routes(): void
+    {
+        $route = new Router();
+
+        try {
+            $route->resources('/users', 'UsersController');
+            $route->resources('/authors', 'AuthorsController');
+
+            $route->resources('/books', 'BooksController');
+            $route->get('/books/tags/id', 'BooksController', 'booksByTagId');
+            $route->get('/books/id/tags', 'BooksController', 'getTags');
+            $route->post('/books/id/tags', 'BooksController', 'addTag');
+            $route->delete('/books/id/tags', 'BooksController', 'removeTag');
+            $route->get('/books/authors/id', 'BooksController', 'booksByAuthor');
+
+            $route->resources('/publishers', 'PublishersController');
+            $route->resources('/tags', 'TagsController');
+        }
+//    catch (PDOException $e) {
+//        echo 'Something went wrong.';
+//    }
+        catch (Exception $e) {
+            echo 'Error ' . $e->getCode() . ': ' . $e->getMessage();
+        }
+    }
+}
--- server.php
+++ server.php
@@ -1,32 +1,8 @@
 <?php
 
 require 'vendor/autoload.php';
-use App\Router\Router;
 
-    $route = new Router();
+use Router\Routes;
 
-    try {
-        $route->resources('/users', 'UsersController');
-        $route->resources('/authors', 'AuthorsController');
+Routes::routes();
 
-        $route->group(
-            [
-                $route->resources('/books', 'BooksController'),
-                $route->get('/books/tags/id', 'BooksController', 'booksByTagId'),
-                $route->get('/books/id/tags', 'BooksController', 'getTags'),
-                $route->post('/books/id/tags', 'BooksController', 'addTag'),
-                $route->delete('/books/id/tags', 'BooksController', 'removeTag'),
-                $route->get('/books/authors/id', 'BooksController', 'booksByAuthor'),
-            ]
-        );
-
-
-        $route->resources('/publishers', 'PublishersController');
-        $route->resources('/tags', 'TagsController');
-    }
-//    catch (PDOException $e) {
-//        echo 'Something went wrong.';
-//    }
-    catch (Exception $e) {
-        echo 'Error ' . $e->getCode() . ': ' . $e->getMessage();
-    }
