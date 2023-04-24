bd8584e0a142279fe097ccb1bbe664d9162fc471
Prisca C <prisca.castanier@gmail.com>
2023-04-24 11:46:54 +0200

    add JWT firebase and dotenv

--- server.php
+++ server.php
@@ -3,6 +3,8 @@
 use Core\Router;
 
 require 'vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
 header("Access-Control-Allow-Origin: *");
 header("Content-Type': 'application/json");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
