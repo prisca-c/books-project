f744f84bc19fa563c020e320d37469d359fd3379
Prisca C <prisca.castanier@gmail.com>
2023-05-02 18:17:17 +0200

    feat(methods): add findBy methods to queryMethods

--- core/database/QueryMethods.php
+++ core/database/QueryMethods.php
@@ -2,31 +2,46 @@
 
 namespace Core\Database;
 
+use MongoDB\InsertOneResult;
+
 
 class QueryMethods extends QueryMethodsExtends
 {
     public static function findAll(string $table): array
     {
-        $query = (new Database)->connect()->__get($table);
+        $query = (new Database)->connect()->$table;
         return $query->find()->toArray();
     }
 
     public static function findById(string $table, int $id): array
     {
-      $query = (new Database)->connect()->__get($table);
+      $query = (new Database)->connect()->$table;
       return $query->find($id)->toArray();
     }
 
     /**
      *
      * @param string $table
-     * @param array $data
-     * @return void
+     * @param string $field
+     * @param string $value
+     * @return array
      */
-    public static function create(string $table, array $data):void
+    public static function findBy(string $table, $field, $value): array
+    {
+      $query = (new Database)->connect()->$table;
+      return $query->find([$field => $value])->toArray();
+    }
+
+    /**
+     *
+     * @param string $table
+     * @param array $data
+     * @return InsertOneResult
+     */
+    public static function create(string $table, array $data): InsertOneResult
     {
       $query = (new Database)->connect()->__get($table);
-      $query->insertOne($data);
+      return $query->insertOne($data);
     }
 
     /**
@@ -38,7 +53,7 @@ class QueryMethods extends QueryMethodsExtends
      */
     public static function update(string $table, array $data, int $id): void
     {
-      $query = (new Database)->connect()->__get($table);
+      $query = (new Database)->connect()->$table;
       $query->updateOne($data, $id);
     }
 
@@ -50,7 +65,7 @@ class QueryMethods extends QueryMethodsExtends
      */
     public static function deleteById(string $table, int $id): void
     {
-      $query = (new Database)->connect()->__get($table);
+      $query = (new Database)->connect()->$table;
       $query->deleteOne($id);
     }
 }
