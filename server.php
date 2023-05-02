86f8d3a1d2ab38844b8166ece3d851ba7c655a26
Prisca C <prisca.castanier@gmail.com>
2023-05-02 16:13:53 +0200

    remove mongo database check

--- server.php
+++ server.php
@@ -2,7 +2,6 @@
 
 require 'vendor/autoload.php';
 
-use Core\Database\Database;
 use Core\EnvLoader;
 use Routes\Middleware;
 use Routes\Routes;
@@ -12,14 +11,3 @@ EnvLoader::envLoader();
 Middleware::headers();
 
 Routes::routes();
-
-require 'vendor/autoload.php';
-
-$database = new Database;
-try {
-  $database->connect()->selectDatabase('books')->command(['ping' => 1]);
-  echo "Pinged your deployment. You successfully connected to MongoDB!\n";
-} catch(Exception $e)
-{
-  echo $e;
-};
