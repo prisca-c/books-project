6ffb52adeddd303d29ce84fea8e50aa2e44f50c1
Prisca C <prisca.castanier@gmail.com>
2023-05-05 10:15:58 +0200

    refactor(rating): rework each method to handle the update of rating, books and user collection

--- app/Controllers/RatingsController.php
+++ app/Controllers/RatingsController.php
@@ -4,6 +4,7 @@ namespace App\Controllers;
 
 use App\Models\Rating;
 use Core\Controller;
+use MongoDB\BSON\ObjectId;
 use MongoDB\Model\BSONDocument;
 
 class RatingsController extends Controller
@@ -26,18 +27,92 @@ class RatingsController extends Controller
         return $this->ratings->findById($id);
     }
 
-    public function store(array $data): void
+    public function store(array $data): array
     {
-        $this->ratings->create($data);
+        $data = $data['data'];
+        $rating = $data['rating'];
+        $review = $data['review'];
+        $book = $data['book'];
+        $user = $data['user'];
+
+        if (empty($rating) || empty($review) || empty($book) || empty($user)) {
+            return $this->response->internalServerError('Missing data');
+        }
+
+        if (count($data) > 4) {
+            return $this->response->internalServerError('Too many data');
+        }
+
+        $rating = $this->ratings->create($data);
+
+        //update reviews.rating to push new rating
+        $this->db->__get('books')->updateOne(
+            ['_id' => new ObjectId($book['_id']['$oid'])],
+            ['$push' => ['reviews.ratings' => $rating->getInsertedId()]]
+        );
+
+        $this->db->__get('users')->updateOne(
+            ['_id' => new ObjectId($user['_id']['$oid'])],
+            ['$push' => ['reviews.ratings' => $rating->getInsertedId()]]
+        );
+
+        // get rating average from a book in ratings collection
+        $ratingAvg = $this->db->ratings->aggregate([
+            ['$match' => ['book' => new ObjectId($book['_id']['$oid'])]],
+            ['$group' => ['_id' => '$book', 'average' => ['$avg' => '$rating']]]
+        ]);
+
+        // update book.rating
+        $this->db->__get('books')->updateOne(
+            ['_id' => new ObjectId($book['_id']['$oid'])],
+            ['$set' => ['rating' => $ratingAvg->toArray()[0]->average]]
+        );
+
+        return $this->response->created();
     }
 
-    public function update(array $data): void
+    public function update(array $data): array
     {
-        $this->ratings->update($data);
+        $data = $data['data'];
+        $id = $data['_id'];
+        $rating = $data['rating'];
+        $review = $data['review'];
+        $book = $data['book'];
+        $user = $data['user'];
+
+        if (empty($rating) || empty($review) || empty($book) || empty($user)) {
+            return $this->response->internalServerError('Missing data');
+        }
+
+        if (count($data) > 5) {
+            return $this->response->internalServerError('Too many data');
+        }
+
+        $ratingAvg = $this->db->ratings->aggregate([
+            ['$match' => ['book' => new ObjectId($book['_id']['$oid'])]],
+            ['$group' => ['_id' => '$book', 'average' => ['$avg' => '$rating']]]
+        ]);
+
+        $this->db->books->updateOne(
+            ['_id' => new ObjectId($book['_id']['$oid'])],
+            ['$set' => ['rating' => $ratingAvg->toArray()[0]->average]]
+        );
+
+        return $this->ratings->update($data);
     }
 
-    public function delete(string $id): void
+    public function delete(string $id): array
     {
-        $this->ratings->deleteById($id);
+        $this->db->books->updateOne(
+            ['reviews.ratings' => new ObjectId($id)],
+            ['$pull' => ['reviews.ratings' => new ObjectId($id)]]
+        );
+
+        $this->db->users->updateOne(
+            ['reviews.ratings' => new ObjectId($id)],
+            ['$pull' => ['reviews.ratings' => new ObjectId($id)]]
+        );
+        
+        return $this->ratings->deleteById($id);
     }
 }
